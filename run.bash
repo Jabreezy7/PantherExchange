b236ea35b66aefa774db925658c9f98df6814963
ItsAndrewC! <andew73885@gmail.com>
2025-11-17 16:24:33 -0500

    added a run and stop bash file

---
+++ run.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+URL="http://localhost:5173"
+
+cd docker/
+docker compose up -d
+cd ..
+
+echo "Waiting for server to be ready..."
+until curl -s --head --request GET "$URL" | grep "200 OK" > /dev/null; do
+    sleep 2
+done
+
+case "$(uname)" in
+    Linux*) xdg-open "$URL" ;;
+    Darwin*) open "$URL" ;;
+    MINGW*|MSYS*|CYGWIN*) start "$URL" ;;
+    *) echo "Unsupported OS" ;;
+esac
